54880cf6cca110aca17ebe0de518a9ac4576ad62
BogdanPanea <tombird83@yahoo.com>
2022-02-23 09:28:47 +0200

    war file

--- src/main/java/ro/bogdanpanea/restWithoutDatabase/RestWithoutDatabaseApplication.java
+++ src/main/java/ro/bogdanpanea/restWithoutDatabase/RestWithoutDatabaseApplication.java
@@ -2,9 +2,10 @@ package ro.bogdanpanea.restWithoutDatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestWithoutDatabaseApplication {
+public class RestWithoutDatabaseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestWithoutDatabaseApplication.class, args);
